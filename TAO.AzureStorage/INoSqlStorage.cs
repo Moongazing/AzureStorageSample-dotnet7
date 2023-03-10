9ab0cff0b2c9b1dd5821ef8de0e6ed0bac270488
Moongazing <72943708+Moongazing@users.noreply.github.com>
2023-03-10 04:30:51 +0300

    Add project files.

---
+++ TAO.AzureStorage/INoSqlStorage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAO.AzureStorage
+{
+    public interface INoSqlStorage<TEntity>
+    {
+        Task<TEntity> Add(TEntity entity);
+        Task Delete(string rowKey, string partitionKey);
+        Task<TEntity> Update(TEntity entity);
+        Task<TEntity> Get(string rowKey, string partitionKey);
+        Task<IQueryable<TEntity>> GetAll();
+        IQueryable<TEntity> Query(Expression<Func<TEntity, bool>> query);
+
+    }
+}
---
+++ TAO.AzureStorage/Services/TableStorage.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAO.AzureStorage.Services
+{
+    public class TableStorage<TEntity> : INoSqlStorage<TEntity> where TEntity : TableEntity, new()
+    {
+        private readonly CloudTableClient _cloudTableClient;
+        private readonly CloudTable _cloudTable;
+        public TableStorage()
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConnectionStrings.AzureStorageConnectionString);
+
+            _cloudTableClient = storageAccount.CreateCloudTableClient();
+            _cloudTable = _cloudTableClient.GetTableReference(typeof(TEntity).Name);
+
+            _cloudTable.CreateIfNotExists();
+        }
+        public Task<TEntity> Add(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(string rowKey, string partitionKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> Get(string rowKey, string partitionKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IQueryable<TEntity>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<TEntity> Query(Expression<Func<TEntity, bool>> query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
