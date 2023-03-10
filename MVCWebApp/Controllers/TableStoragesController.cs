ece9bb0170566a50424271d416594a7583ffa4db
Moongazing <72943708+Moongazing@users.noreply.github.com>
2023-03-10 18:33:06 +0300

    Adding MVC Project, Adding TableStoragesController, Adding Connection strings Design Index Page

---
+++ MVCWebApp/Controllers/TableStoragesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TAO.AzureStorage;
+using TAO.AzureStorage.Model;
+
+namespace MVCWebApp.Controllers
+{
+    public class TableStoragesController : Controller
+    {
+        private readonly INoSqlStorage<Product> _noSqlStorage;
+        public TableStoragesController(INoSqlStorage<Product> noSqlStorage)
+        {
+            _noSqlStorage = noSqlStorage;
+        }
+        public IActionResult Index()
+        {
+
+            ViewBag.products = _noSqlStorage.GetAll().ToList();
+            return View();
+        }
+    }
+}
